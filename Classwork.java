21a93849892876ab6e376d1d68955ea058e8934e
LHNT2k7 <ltri9573@gmail.com>
2024-09-27 09:54:44 -0700

    1

--- Classwork.java
+++ Classwork.java
@@ -2,7 +2,11 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN(word:"hfhsef", n:2));
+  }
+  public static Sring firstLastN(String word, int n)
+  {
+    String output = new String();
   }
 
   // write solutions to problems below
